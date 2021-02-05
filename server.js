6d3f3ca2cbb7b7c5cc2945f9d969891db0234f80
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2021-02-05 12:35:52 +0530

    Added sessions

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
+require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const redis = require("redis");
+const RedisStore = require("connect-redis")(session);
+
+const { COOKIE_SECRET, NODE_ENV, PORT, REDIS_URL } = process.env;
+
 const app = express();
+if (NODE_ENV !== "development") {
+  app.set("trust proxy", 1);
+}
+
+const redisClient = redis.createClient(REDIS_URL);
+app.use(
+  session({
+    name: "ae-sid",
+    cookie: {
+      httpOnly: true,
+      maxAge: 100 * 365 * 86400 * 1000, // persists a 100 years
+      secure: NODE_ENV !== "development",
+      sameSite: NODE_ENV !== "development" ? "none" : "strict",
+    },
+    resave: false,
+    saveUninitialized: true,
+    secret: COOKIE_SECRET,
+    store: new RedisStore({ client: redisClient }),
+  })
+);
 
 app.get("/", (req, res) => res.end("OK"));
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running"));
+app.listen(PORT, () => console.log("Server running"));
