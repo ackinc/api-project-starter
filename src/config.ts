7273bdcbaa28f71aeb3be817e0bb40a31ae71013
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2021-02-22 16:57:15 +0530

    Removed unused env vars

--- src/config.ts
+++ src/config.ts
@@ -8,10 +8,6 @@ const {
   NODE_ENV = "development",
   PORT = 3000,
   REDIS_URL,
-  SMTP_HOST,
-  SMTP_PORT,
-  SMTP_USER,
-  SMTP_PASSWORD,
   SMTP_URL,
 } = process.env;
 if (!ALLOWED_ORIGINS) console.warn("Warning: No ALLOWED_ORIGINS in env");
@@ -24,9 +20,7 @@ if (!FRONTEND_LOCATION) throw new Error("No FRONTEND_LOCATION in env");
 if (ALLOWED_ORIGINS && !ALLOWED_ORIGINS.split(",").includes(FRONTEND_LOCATION))
   console.warn("Warning: FRONTEND_LOCATION not in ALLOWED_ORIGINS");
 if (!REDIS_URL) throw new Error("No REDIS_URL in env");
-if (!(SMTP_HOST && SMTP_PORT && SMTP_USER && SMTP_PASSWORD) || !SMTP_URL) {
-  throw new Error("Missing SMTP env vars");
-}
+if (!SMTP_URL) throw new Error("No SMTP_URL in env");
 
 export const allowedOrigins = ALLOWED_ORIGINS?.split(",");
 export const apiLocation = API_LOCATION;
@@ -37,10 +31,6 @@ export const frontendLocation = FRONTEND_LOCATION;
 export const nodeEnv = NODE_ENV;
 export const port = PORT;
 export const redisUrl = REDIS_URL;
-export const smtpHost = SMTP_HOST;
-export const smtpPort = Number(SMTP_PORT);
-export const smtpUser = SMTP_USER;
-export const smtpPassword = SMTP_PASSWORD;
 export const smtpUrl = SMTP_URL;
 
 export const emailVerificationTokenLength = 30;
